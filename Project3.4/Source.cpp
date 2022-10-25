caab7ee83a89e665a58c5a6067fe671ed87f5f07
tlyyn <lin.tarass@gmail.com>
2022-10-26 02:15:23 +0300

    first commit

---
+++ Project3.4/Source.cpp
@@ -0,0 +1,24 @@
+﻿// Lab_03_4.cpp
+// Лин Тарас
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 13
+#include <iostream>
+using namespace std;
+int main()
+{
+	double y; // вхідний параметр
+	double R1; // вхідний параметр
+	double R2; // вхідний параметр
+	cout << "y = "; cin >> y;
+	cout << "R1 = "; cin >> R1;
+	cout << "R2 = "; cin >> R2;
+
+	// розгалуження в повній формі
+	if ((y <= R2 && y >= 0)  || (y <= 0 && y >= R1))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
